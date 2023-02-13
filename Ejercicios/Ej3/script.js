6f48f05891cd5b914681f2cf84ff6e8e8178e8ed
German-devv <germanurgorri@gmail.com>
2023-02-13 14:00:59 +0000

    13/02

--- Ejercicios/Ej3/script.js
+++ Ejercicios/Ej3/script.js
@@ -1,23 +1,13 @@
 const image = document.getElementById('image');
 const moveButton = document.getElementById('Mover');
 const resetButton = document.getElementById('Situar');
-const section = document.getElementById('section').offsetWidth;
-
-
-moveButton.addEventListener('click', () => mover());
-
-resetButton.addEventListener('click', function() {
-  image.style.transition = "all 0s";
-  image.style.transform = "translateX(0px)";
-});
 
 
 async function mover(){
+const section = document.getElementById('section').offsetWidth;
     image.style.transition = "all 3s";
-    image.style.transform = `translateX(${section - image.offsetWidth}px)`;
+    image.style.transform = `translateX(${section - image.offsetWidth - 25}px)`;
     
-
-
   setTimeout(function() {
     image.style.transition = "all 3s";
 
@@ -25,3 +15,13 @@ async function mover(){
     image.style.filter = "none";
   }, 3000);
 }
+resetButton.addEventListener('click', function() {
+  image.style.transition = "all 0s";
+  image.style.transform = "translateX(0px)";
+});
+
+
+moveButton.addEventListener('click', () => mover());
+
+
+
